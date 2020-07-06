d44875e5e3d9339dac1ad78ddf1875d31c4e5e27
miguelmoreirabandtec <miguel.moreira@bandtec.com.br>
2020-07-06 16:59:18 -0300

    Introdução ao ES6

---
+++ Modulo-2-curso/js/1-variaveis.js
@@ -0,0 +1,14 @@
+// escopo global
+
+// {
+//     //escopo bloco
+// }
+
+
+var nameVar = 'Guilherme';
+let nameLet = 'Guilherme';
+const nameConst = 'Guilherme';
+
+console.log(`nameVar: ${nameVar}`);
+console.log(`nameLet: ${nameLet}`);
+console.log(`nameConst: ${nameConst}`)
